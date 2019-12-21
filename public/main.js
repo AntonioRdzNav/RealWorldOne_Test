4f9e10b5ff9383a836309db33641e8c0cc66583c
AntonioRdzNav <antoniordznav@outlook.com>
2019-12-21 20:59:53 +0100

    Everything styled and ready to deploy

--- public/main.js
+++ public/main.js
@@ -1,2 +1,5 @@
 
-$('.alert').delay(2000).fadeOut(300);
+$('.alert').delay(2000).fadeOut(300, function() { $(this).remove(); });
+
+var textarea = document.getElementById('chat');
+textarea.scrollTop = textarea.scrollHeight;
